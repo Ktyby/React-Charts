dc0bf6178f6a139679d47f0a7f50b2bd0e58012e
Ktyby <zakhar.kutsko@instinctools.com>
2021-05-15 10:14:02 +0300

    add left axis

--- src/components/BarChart/BottomAxis/BottomAxis.jsx
+++ src/components/BarChart/BottomAxis/BottomAxis.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 const BottomAxis = memo(({ domain }) => (
-	<div className="chart__axis">
+	<div className="chart__bottom-axis">
 		{domain.map((element, index) => (
 			<div className="chart__interval" key={index}>
 				{element}
---
+++ src/components/BarChart/LeftAxis/LeftAxis.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style.css';
+
+const LeftAxis = () => {
+	return <div className="chart__left-axis"></div>;
+};
+
+export default LeftAxis;
